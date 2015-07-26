91e24a22e51300f3fca92a1e00851271d2d41853
Alex Adamczyk <alextigers@gmail.com>
2015-07-26 19:09:28 -0400

    Completed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Returns the inverse of a matrix
+## Caches the inverse when it is calculated
+## Only calculates the inverse if it is not currently cached
 
-## Write a short comment describing this function
+
+## Calculates the inverse of a matrix and caches the matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  globalMatrix <<- x
+  globalInverse <<- solve(x)
 }
 
 
-## Write a short comment describing this function
+## Tests if the matrix matches the cached matrix, if not runs makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if (!exists("globalMatrix")) {  ## Tests if a matrix has been cached
+    globalMatrix <- NULL
+  }
+  if (identical(x, globalMatrix)) {  ## Tests if cached matrix matches the matrix being inverted
+    message("getting cached matrix")
+  } else {
+    makeCacheMatrix(x)
+  }
+  return (globalInverse)
 }
